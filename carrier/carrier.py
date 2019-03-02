04c2b00bf42f39110a05dabbf33511628c8a0232
Ivan Krakhmaliuk (LifeDJIK) <lifedj27@gmail.com>
2019-03-02 13:54:46 +0200

    Another structure candidate

--- carrier/carrier.py
+++ carrier/carrier.py
@@ -1,11 +1,35 @@
 #!/usr/bin/python3
 # coding=utf-8
 
+#   Copyright 2019 getcarrier.io
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+"""
+    Main entry point
+"""
+
 import logging
 
 
 def main():
     """ Main """
     # Setup logging
-    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)8s - %(message)s', datefmt='%Y.%m.%d %H:%M:%S')
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format='%(asctime)s - %(levelname)8s - %(message)s',
+        datefmt='%Y.%m.%d %H:%M:%S'
+    )
     logging.info("Welcome to carrier")
+    #
+    
